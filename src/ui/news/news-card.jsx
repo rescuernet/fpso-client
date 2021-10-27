f267961ca428736e0073069ebf75deed0e02af43
rescuernet <om-s@list.ru>
2021-10-27 23:19:18 +0400

    1

--- src/ui/news/news-card.jsx
+++ src/ui/news/news-card.jsx
@@ -62,7 +62,7 @@ export const NewsCard = ({news})=> {
                 <Button
                     size="small"
                     color="primary"
-
+                    onClick={}
                 >
                     Подробнее..
                 </Button>
