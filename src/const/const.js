9805746974e9554c8e91e11e0f8306e16fa74197
rescuernet <om-s@list.ru>
2021-12-20 14:31:46 +0400

    1

--- src/const/const.js
+++ src/const/const.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL || `http://localhost:5000`
+export const API_URL = process.env.REACT_APP_API_URL || `http://localhost:5000`
 
 export const BACKGROUD_COLOR_BLUE = '#3f51b5'
